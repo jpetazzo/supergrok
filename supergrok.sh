26d525c341d593da3549c25f00a772332f4962e4
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2016-11-18 09:24:26 -0800

    Improve supergrok message

--- supergrok.sh
+++ supergrok.sh
@@ -9,7 +9,25 @@ while true; do
   [ "$URL" ] && break
   sleep 1
 done
-echo "Your supergrok tunnelizer is available at: $URL"
-echo "And here it is in machine-catchable format:"
-echo "URL=$URL"
+cat <<EOF
+
+Your supergrok tunnelizer is available at: $URL
+
+If you go to that URL, you should see the default "Welcome to NGINX!" page.
+To connect to one of your Docker nodes, add the IP address of that node (and
+an optional port number) after that URL. For instance, to reach the service
+running on node 10.0.3.5 and exposed on port 8000, just go to the following
+address: http://xxxx.ngrok.io/10.0.3.5:8000 
+
+Note that NGINX will not rewrite absolute links contained in your pages. In
+other words, if you serve a web page containing a link to "/hello", it will
+translate to http://xxxx.ngrok.io/hello, which will not work. The correct
+link would be http://xxxx.ngrok.io/10.0.3.5:8000/hello! However, NGINX will
+rewrite "Location:" headers, so simple HTTP redirections should work.
+
+Here is the URL in a machine-catchable format (so you can grep ^URL= these
+messages, if you are so inclined):
+
+URL=$URL
+EOF
 wait
